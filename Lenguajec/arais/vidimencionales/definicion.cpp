1d47487f6516a1e300d1f31cdde1ce219aff8e4a
alexis chicaiza <michaelch3002@gmail.com>
2025-01-20 10:06:36 -0500

    cambios

--- Lenguajec/arais/vidimencionales/definicion.cpp
+++ Lenguajec/arais/vidimencionales/definicion.cpp
@@ -3,7 +3,7 @@
 #include<iostream>
 using namespace std;
 main(){
-    int tabla[3][3];
+    int tabla[3][3]; 
     float pesos[2][3]={{1.2,3.4,5.6},{7.8,9.0,1.2}};
     cout<<pesos[0][0]<<endl;
     cout<<pesos[1][1]<<endl;
