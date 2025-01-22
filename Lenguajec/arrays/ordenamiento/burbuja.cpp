a80ed8e9823bd04dd8689dc1117844e0d8474840
alexis chicaiza <michaelch3002@gmail.com>
2025-01-22 10:53:13 -0500

    commit

---
+++ Lenguajec/arrays/ordenamiento/burbuja.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<stdlib.h>
+#include<time.h>
+using namespace std;
+void llenarVector(int v[],int n){
+    srand(time(NULL));
+    for(int i=0;i<n;i++){
+        v[i]=rand()%20+1;
+    }
+}
+void ordenarBurbuja(int v[], int n){
+    int aux;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-1;j++){
+                if(v[j]<v[j+1]){
+                    aux=v[j];
+                    v[j]=v[j+1];
+                    v[j+1]=aux;
+                }
+        }
+    }
+
+}
+void verDatos(int v[], int ne){
+    cout<<"Los elemenetos del vector son: "<<endl;
+    for(int i=0;i<ne;i++){
+        cout<<v[i]<<" ";
+    }
+
+}
+main(){
+    int ne;
+    cout<<"Numero de elementos: ";
+    cin>>ne;
+int vec[ne];
+llenarVector(vec,ne);
+cout<<"Vector original: "<<endl;
+verDatos(vec,ne);
+ordenarBurbuja(vec,ne);
+cout<<"Vector ordenado: "<<endl;
+verDatos(vec,ne);
+}
