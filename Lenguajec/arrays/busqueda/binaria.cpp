aad32d0e254bc19571a7f6460057771eb85b56e3
alexis chicaiza <michaelch3002@gmail.com>
2025-02-05 10:33:57 -0500

    ordenamiento

--- Lenguajec/arrays/busqueda/binaria.cpp
+++ Lenguajec/arrays/busqueda/binaria.cpp
@@ -2,61 +2,17 @@
 #include"../../librerias/arrays.h"
 using namespace std;
 using namespace vectorn;
-bool isEncontradov1(int v[], int n, int elemento)
-{
-    bool encontrado = false;
-    for (int i = 0; i < n; i++)
-    {
-        if (v[i] == elemento)
-        {
-            encontrado = true;
-            break;
-        }
-    }
-    return encontrado;
-}
-bool isEncontradov2(int v[], int n, int elemento)
-{
-    // bool encontrado=false;
-    for (int i = 0; i < n; i++)
-    {
-        if (v[i] == elemento)
-        {
-            // encontrado=true;
-            // break;
-            return true;
-        }
-    }
-    return false;
-}
-bool isBusquedab(int v[], int n, int elemento)
-{
-    int inf = 0, sup = n - 1, mitad;
-    while (inf <= sup)
-    {
-        mitad = (inf + sup) / 2;
-        if (v[mitad] == elemento)
-        {
-            return true;
-        }
-        else if (elemento < v[mitad])
-        {
-            sup = mitad - 1;
-        }
-        else
-        {
-            inf = mitad + 1;
-        }
-    }
-    return false;
-}
 main()
 {
-    int ne,dato;
+    int ne,elemento;
     cout<<"Numero de elementos: ";
     cin>>ne;
     int vector[ne];
+    cout << "Ingresa el datos a buscar :"; 
+    cin >> elemento;
     llenarVector(vector,ne);
     ordenarBurbujav3(vector,ne);
+    verVector(vector,ne);
+    (isBusquedab(vector,ne,elemento)) ? cout << "\nEl elemento esta en el array" : cout << "\nEl elemento no existe";
     verVector(vector,ne);
 }
