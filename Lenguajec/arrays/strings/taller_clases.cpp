ce49e986739a2cb40ff4d7051887227a437fc8fe
alexis chicaiza <michaelch3002@gmail.com>
2025-02-17 09:42:08 -0500

    reemplazar_palabras

--- Lenguajec/arrays/strings/taller_clases.cpp
+++ Lenguajec/arrays/strings/taller_clases.cpp
@@ -1,5 +1,26 @@
 #include<iostream>
 using namespace std;
+void reemplazar(string &cadena){
+    string cadena2, cadena3;
+    int pos,nencontrado=0;
+    char respuesta;
+    cout << "Ingrese cadena objetivo: ";
+    getline(cin, cadena2);
+    cout << "Ingrese cadena para reemplazar: ";
+    getline(cin, cadena3);
+    pos = cadena.find(cadena2);
+    while (pos != string::npos) {
+        nencontrado++;
+        cout<<"Lo buscado esta en la posicion "<<nencontrado<<"  Desea Reemplazarlo?<<s/n: ";
+        respuesta = cin.get();
+        cin.ignore();  
+        if(respuesta == 's' || respuesta == 'S'){
+            cadena.replace(pos, cadena2.size(), cadena3);      
+        }
+        pos = cadena.find(cadena2, pos + cadena3.size());
+    }  
+
+}
 void cambiarString(string &nombres){
     string nueva;
     string nueva1;
@@ -22,5 +43,7 @@ int main(){
     string cadena;
     cout<<"Ingrese una cadena: ";
     getline(cin, cadena);
-    cambiarString(cadena);
+    //cambiarString(cadena);
+    reemplazar(cadena);
+    cout<<"La cadena es: "<<cadena<<endl;
 }
